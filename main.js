7f7080d80a0a8f326f68131eaab15c1d3f0917f6
lisseloth16 <agustinvillablanca@insucodos.cl>
2025-07-11 11:38:03 -0400

    Feature: create post

---
+++ main.js
@@ -0,0 +1,12 @@
+const formulario = document.querySelector(".form-create");
+formulario.addEventListener("submit",(e)=> {
+    e.preventDefault();
+    const publicacion = document.querySelector("#publicacion").value;
+    const contenedor = document.querySelector(".contenedor-publicaciones");
+    contenedor.innerHTML += `<div class="publicacion">
+            <p>id: 00</p>
+            <p>${publicacion}</p>
+            <button>Editar</button>
+            <button>Eliminar</button>
+        </div>`;
+}); 
